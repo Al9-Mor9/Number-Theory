34fd815439151d64c863c0d0c191ef7aeb807435
Haaarimmm <haaarimmm@gmail.com>
2023-02-11 23:57:17 +0900

    4375_K.py

---
+++ Code/4375/4375_K.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+
+while True:
+    try: 
+        n = int(input())
+    except: 
+        break
+    s = 0 # sum
+    i = 0 # 자릿수
+    while True:
+        i += 1
+        s = s * 10 + 1
+        if s % n == 0:
+            print(i)
+            break
