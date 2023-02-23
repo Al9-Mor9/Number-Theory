5a098c5efe46ef33cdea1d6deb02abc6cef2b585
wocjs <wocjs602@gmail.com>
2023-02-23 18:18:57 +0900

    h

---
+++ Code/10610/10610_H.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open('text.txt', 'r')
+#####
+n = list(input())
+_n = []
+s = 0
+ans = []
+
+for i in n:
+    i = int(i)
+    _n.append(i)
+    s += i
+
+if not s // 3 or '0' not in n:
+    print(-1)
+    exit()
+n = sorted(n, reverse=True)
+print(''.join(n))
