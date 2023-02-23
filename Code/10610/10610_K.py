2600a6eb140c261abe8e15b0b7c5a622a1c78131
Haaarimmm <haaarimmm@gmail.com>
2023-02-23 16:50:50 +0900

    10610_K.py

---
+++ Code/10610/10610_K.py
@@ -0,0 +1,14 @@
+# 30
+import sys
+sys.stdin = open('input.txt', 'r')
+
+arr = list(map(int, input()))
+
+if 0 not in arr or len(arr) == 1:
+    print(-1)
+else:
+    if sum(arr) % 3:
+        print(-1)
+    else:
+        arr.sort(reverse=True)
+        print(''.join(map(str, arr)))
